e0cc79dad9f2eb7ef97d4794f35f296e535c3ba4
Peter Brown <phbrown@acm.org>
2024-04-25 15:53:39 -0400

    ARC's filtered from the end back through MATH

--- filters/FilterARCs.py
+++ filters/FilterARCs.py
@@ -18,7 +18,31 @@ ARC_equivalences = {'ART0101': 'ART100',
                     'FREN102': 'FRN102', # Elementary French
                     'FREN201': 'FRN201', # More French
                     'FRLGN001': 'ASL101', # Only happens with ASL
+                    
                     # There are a good many more of these
+                    
+                    'MATH108': 'MTH108',
+                    'MATH110': 'MTH110',
+                    'MATH1113': 'MTH113',
+                    'MATH113': 'MTH113',
+                    'MATH115': 'MTH115', # Calculus One
+                    'MATH221': 'MTH120', # Analytical Geometry & Calculus
+                    'MTH00': 'MTH110', # Elementary Functions
+                    'MTH11': 'MTH105', # College Algebra
+                    'POL100': 'POL101', # US Government & Politics
+                    'PS101': 'PSY101', # General Psychology
+                    'PSY00': 'PSY101', # General Psychology
+                    'SOC00': 'SOC101', # Intro to Sociology
+                    'SOC0101': 'SOC101', # Intro to Sociology
+                    'SPA101': 'SPN101', # Elementary Spanish I
+                    'SPA102': 'SPN102', # Elementary Spanish II
+                    'SPA201': 'SPN201', # Intermediate Spanish
+                    'SPA202': 'SPN202', # Intermediate Spanish
+                    'SPAN201': 'SPN201', # Intermediate Spanish I
+                    'SPAN202': 'SPN202', # Spanish IV
+                    'THE1000': 'THR100', # Intro to Theatre
+                    'THE1150': 'THR115', # Fundamentals of Acting
+                    'TRANS001': 'TRAN001' # "Transfer Work"
                     }
 
 def filter_arcs(transfer_credits: list[dict[str,str]]) -> list[dict[str,str]]:
