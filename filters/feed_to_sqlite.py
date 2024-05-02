0021fef5186b1a3b9ecd62b63fdef1b502098e11
Peter Brown <phbrown@acm.org>
2024-05-02 15:05:33 -0400

    feed_to_sqlite changes

--- filters/feed_to_sqlite.py
+++ filters/feed_to_sqlite.py
@@ -1,15 +1,29 @@
 import sqlite3
+from pathlib import Path
 
-#Connect to the SQLite database
-conn = sqlite3.connect('database file name') 
-cursor = conn.cursor()
+def connectDB(db_fname: Path) -> sqlite3.Connection:
+    #Connect to the SQLite database
+    conn = sqlite3.connect(db_fname) 
+    return conn
+
+def getCursor(conn: sqlite3.Connection) -> sqlite3.Cursor:
+    return conn.cursor()
 
 #Execute the SQL DELETE statement to delete all rows in a table
 # cursor.execute("DELETE FROM table_name")
 #(repeat this for all tables)
 
-#Commit the statement
-conn.commit()
+def closeDB(conn: sqlite3.Connection) -> None:
 
-#Close the connection
-conn.close() 
+    #Close the connection
+    conn.close() 
+
+def main(args: list[str]) -> int:
+    conn = connectDB
+
+
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
