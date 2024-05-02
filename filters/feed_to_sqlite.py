0593c0afc2a1718e9985e8e9a6e49323705636d0
jhardy001 <jhardy001@converse.edu>
2024-05-02 14:50:24 -0400

    Added Licoln's code to feed_to_sqlite

--- filters/feed_to_sqlite.py
+++ filters/feed_to_sqlite.py
@@ -1,2 +1,15 @@
 import sqlite3
 
+#Connect to the SQLite database
+conn = sqlite3.connect('database file name') 
+cursor = conn.cursor()
+
+#Execute the SQL DELETE statement to delete all rows in a table
+# cursor.execute("DELETE FROM table_name")
+#(repeat this for all tables)
+
+#Commit the statement
+conn.commit()
+
+#Close the connection
+conn.close() 
